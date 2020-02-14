93e40190d8bfaedb6701595c0ccc9894af9f6fea
masahiro.tsuji <ma.cag08hsp02@gmail.com>
2020-02-15 02:16:54 +0900

    Add database files

---
+++ src/models/Foreign.java
@@ -0,0 +1,81 @@
+package models;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "foreigns")
+public class Foreign {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "title", length = 225, nullable = false)
+    private String title;
+
+    @Column(name = "content", length = 225, nullable = false)
+    private String content;
+
+    @Column(name = "str", length = 225, nullable = false)
+    private String str;
+
+    @Column(name = "lat", length = 225, nullable = false)
+    private double lat;
+
+    @Column(name = "lng", length = 225, nullable = false)
+    private double lng;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public String getStr() {
+        return str;
+    }
+
+    public void setStr(String str) {
+        this.str = str;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public double getLat() {
+        return lat;
+    }
+
+    public void setLat(double lat) {
+        this.lat = lat;
+    }
+
+    public double getLng() {
+        return lng;
+    }
+
+    public void setLng(double lng) {
+        this.lng = lng;
+    }
+
+}
